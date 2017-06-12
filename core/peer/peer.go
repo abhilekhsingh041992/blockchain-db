a304d2fc13388064b2c633e021e92b7c30f81a4b
abhilekhsingh041992 <abhilekhsingh041992@gmail.com>
2017-06-12 12:45:49 +0400

    Added peer type, add peer and get peers methods

---
+++ core/peer/peer.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+
+)
+
+
+type PeerInfo struct {
+	Peers []Peer
+}
+
+type Peer struct {
+	address string
+	port string
+}
+
+func (peerInfo *PeerInfo) getPeers() []Peer {
+	return peerInfo.Peers
+}
+
+func (peerInfo *PeerInfo) AddPeer(address string, port string) (*Peer, error) {
+	peer := &Peer{}
+	peer.address = address
+	peer.port = port
+	peerInfo.Peers = append(peerInfo.Peers, *peer)
+	return peer, nil
+}
